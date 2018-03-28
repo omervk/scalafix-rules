cec7202ccbabce94b502097064797d9ef7c03c9c
Omer van Kloeten <omer.van.kloeten@gmail.com>
2018-03-29 00:15:15 +0300

    Added test sto make sure PublicInference covers implicit instances as well

--- scalafix/input/src/main/scala/fix/PublicInference_Test.scala
+++ scalafix/input/src/main/scala/fix/PublicInference_Test.scala
@@ -8,12 +8,20 @@ object PublicInference_Test {
   var test2 = "" // assert: PublicInference
   lazy val test3 = "" // assert: PublicInference
   def test4 = "" // assert: PublicInference
+  implicit val test5 = 1 // assert: PublicInference
+  implicit var test6 = 1.1 // assert: PublicInference
+  implicit lazy val test7 = None // assert: PublicInference
+  implicit def test8 = List.empty // assert: PublicInference
 
   private object Private {
     val test1 = ""
     var test2 = ""
     lazy val test3 = ""
     def test4 = ""
+    implicit val test5 = 1
+    implicit var test6 = 1.1
+    implicit lazy val test7 = None
+    implicit def test8 = List.empty
   }
 
   protected class Protected {
@@ -21,6 +29,10 @@ object PublicInference_Test {
     var test2 = "" // assert: PublicInference
     lazy val test3 = "" // assert: PublicInference
     def test4 = "" // assert: PublicInference
+    implicit val test5 = 1 // assert: PublicInference
+    implicit var test6 = 1.1 // assert: PublicInference
+    implicit lazy val test7 = None // assert: PublicInference
+    implicit def test8 = List.empty // assert: PublicInference
   }
 
   private[fix] object PrivatePackage {
@@ -28,6 +40,10 @@ object PublicInference_Test {
     var test2 = "" // assert: PublicInference
     lazy val test3 = "" // assert: PublicInference
     def test4 = "" // assert: PublicInference
+    implicit val test5 = 1 // assert: PublicInference
+    implicit var test6 = 1.1 // assert: PublicInference
+    implicit lazy val test7 = None // assert: PublicInference
+    implicit def test8 = List.empty // assert: PublicInference
   }
 
   class Public {
@@ -35,11 +51,18 @@ object PublicInference_Test {
     var test2 = "" // assert: PublicInference
     lazy val test3 = "" // assert: PublicInference
     def test4 = "" // assert: PublicInference
+    implicit val test5 = 1 // assert: PublicInference
+    implicit var test6 = 1.1 // assert: PublicInference
+    implicit lazy val test7 = None // assert: PublicInference
+    implicit def test8 = List.empty // assert: PublicInference
   }
 
   class Overriding extends Public {
     override val test1 = "" // assert: PublicInference
     override lazy val test3 = "" // assert: PublicInference
     override def test4 = "" // assert: PublicInference
+    override implicit val test5 = 1 // assert: PublicInference
+    override implicit lazy val test7 = None // assert: PublicInference
+    override implicit def test8 = List.empty // assert: PublicInference
   }
 }
